1e14bd16922b25d0b518e36b9139d59052b66be4
madHEYsia <submiitr07@gmail.com>
2017-09-01 14:21:34 +0530

    junk - WIP

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(
     connection(mysql,{
         host     : 'localhost',
         user     : 'root',
-        password : 'root',
+        password : 'roowt',
         database : 'classroomshoppers',
         debug    : false //set true if you wanna see debug logger
     },'request')
